8ae8d14a1f624d851afcc4ed52aff9ca6170ef42
Fil <enin.ss.nsk@gmail.com>
2025-04-01 15:20:42 +0700

    1

--- src/main/java/com/filantrop/opensearchExample/Main.java
+++ src/main/java/com/filantrop/opensearchExample/Main.java
@@ -3,8 +3,10 @@ package com.filantrop.opensearchExample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @SpringBootApplication(exclude = ElasticsearchDataAutoConfiguration.class)
+@EnableElasticsearchRepositories
 public class Main {
 
 	public static void main(String[] args) {
--- src/main/java/com/filantrop/opensearchExample/config/OpenSearchConfiguration.java
+++ src/main/java/com/filantrop/opensearchExample/config/OpenSearchConfiguration.java
@@ -1,23 +1,18 @@
 package com.filantrop.opensearchExample.config;
 
-import com.filantrop.opensearchExample.repository.MarketplaceRepository;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.opensearch.client.RestClientBuilder;
 import org.opensearch.spring.boot.autoconfigure.RestClientBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 
 @Configuration
-@EnableElasticsearchRepositories(basePackageClasses = MarketplaceRepository.class)
-@ComponentScan(basePackageClasses = OpenSearchConfiguration.class)
 public class OpenSearchConfiguration {
 
     @Bean
